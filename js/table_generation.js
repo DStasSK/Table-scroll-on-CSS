2808c87c1b4aa36f6d2693dd04125b555ab5e249
DStasSK <DStasSK@yandex.ru>
2026-03-09 11:41:17 +0300

    make it shorter and clearer

--- js/table_generation.js
+++ js/table_generation.js
@@ -1,12 +1,16 @@
 // генерация заголовков таблиц
 let th = '', th2 = '';
+// первая строка заголовка
 for(let i = 1; i < 21; i++){th += '<th>' + i + '</th>';}
+// вторая строка заголовка
 for(let i = 1; i < 11; i++){th2 += '<th colspan="2">' + i + '</th>';}
 
-// генерация содержимого таблици
+
+
+// генерация содержимого таблицы
 const k = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 let fillTbody = '';
-for(let j = 0; j < 14; j++){
+for(let j = 0; j < 26; j++){
    fillTbody += '<tr><td>' + k[j] + '</td>';
    for(let t = 1; t < 21; t++){
       fillTbody += '<td>' + k[j] + t + '</td>';
@@ -14,11 +18,16 @@ for(let j = 0; j < 14; j++){
    fillTbody += '</tr>';
 }
 
-// сборка таблиц
-let table = '<tbody><tr><th>~</th>' + th + '</tr>' + fillTbody + '</tbody>';
-let table2 = '<tbody><tr><th rowspan="2">~</th>' + th2 + '</tr><tr>' + th + '</tr></tbody><tbody>' + fillTbody + '</tbody>';
 
-// экспорт таблиц в тело документа
+
+// сборка элемента tbody для таблиц
+let table = '<tbody><tr><th>~</th>' + th + '</tr>' + fillTbody + '</tbody>';
+// таблица для сложного заголовка
+let table2 = '<thead><tr><th rowspan="2">~</th>' + th2 + '</tr><tr>' + th + '</tr></thead><tbody>' + fillTbody + '</tbody>';
+
+
+
+// вставка таблиц в тело документа
 const arrayOfTable = document.getElementsByName("table");
 const arrayOfTable2 = document.getElementsByName("table_2_body");
 for(let i = 0; i < arrayOfTable.length; i++){
