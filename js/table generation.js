866adfdb4ce285959798307c91fef8dee6b3cc16
DStasSK <DStasSK@yandex.ru>
2026-03-09 11:38:46 +0300

    Revert "fix CSS and optomize DOM"
    This reverts commit 7f3134bd2f017be7f8c1c64314cac339c8a0c7c8.

--- js/table generation.js
+++ js/table generation.js
@@ -1,16 +1,12 @@
 // генерация заголовков таблиц
 let th = '', th2 = '';
-// первая строка заголовка
 for(let i = 1; i < 21; i++){th += '<th>' + i + '</th>';}
-// вторая строка заголовка
 for(let i = 1; i < 11; i++){th2 += '<th colspan="2">' + i + '</th>';}
 
-
-
-// генерация содержимого таблицы
+// генерация содержимого таблици
 const k = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 let fillTbody = '';
-for(let j = 0; j < 26; j++){
+for(let j = 0; j < 14; j++){
    fillTbody += '<tr><td>' + k[j] + '</td>';
    for(let t = 1; t < 21; t++){
       fillTbody += '<td>' + k[j] + t + '</td>';
@@ -18,16 +14,11 @@ for(let j = 0; j < 26; j++){
    fillTbody += '</tr>';
 }
 
-
-
-// сборка элемента tbody для таблиц
+// сборка таблиц
 let table = '<tbody><tr><th>~</th>' + th + '</tr>' + fillTbody + '</tbody>';
-// таблица для сложного заголовка
-let table2 = '<thead><tr><th rowspan="2">~</th>' + th2 + '</tr><tr>' + th + '</tr></thead><tbody>' + fillTbody + '</tbody>';
+let table2 = '<tbody><tr><th rowspan="2">~</th>' + th2 + '</tr><tr>' + th + '</tr></tbody><tbody>' + fillTbody + '</tbody>';
 
-
-
-// вставка таблиц в тело документа
+// экспорт таблиц в тело документа
 const arrayOfTable = document.getElementsByName("table");
 const arrayOfTable2 = document.getElementsByName("table_2_body");
 for(let i = 0; i < arrayOfTable.length; i++){
